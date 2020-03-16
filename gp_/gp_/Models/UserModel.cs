b64c498654e00aa20d2ef03509ee6bcff63e2848
Yousef debaz <y.debaz@hotmail.com>
2020-03-16 13:51:24 +0200

    created userreg mod/ctrl/view 121

---
+++ gp_/gp_/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gp_.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string password { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public System.DateTime? EmailConfirmationDate { get; set; }
+       // public int Score { get; set; }
+    }
+}
