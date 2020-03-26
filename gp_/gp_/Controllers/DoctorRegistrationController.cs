87e197de8c8c458744d860f0ea64dba775bb1dba
Yousef debaz <y.debaz@hotmail.com>
2020-03-26 22:54:35 +0200

    doc reg cont

--- gp_/gp_/Controllers/DoctorRegistrationController.cs
+++ gp_/gp_/Controllers/DoctorRegistrationController.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gp_.Services;
 using Microsoft.AspNetCore.Mvc;
+using gp_.Models;
 
 namespace gp_.Controllers
 {
     public class DoctorRegistrationController : Controller
     {
+
+        private IDoctorService _docService;
+
+        public DoctorRegistrationController(IDoctorService docService)
+        {
+            _docService = docService;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(DoctorModel docModel)
+        {
+            if (ModelState.IsValid)
+            {
+                await _docService.RegisterDoc(docModel);
+                return RedirectToAction(nameof(Emailconfirmation), new { docModel.Email });
+                // return Content($"User {userModel.FirstName}  {userModel.LastName} has been registered successfully");
+            }
+            else
+            {
+                return View(docModel);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Emailconfirmation(string email)
+        {
+            ViewBag.Email = email;
             return View();
         }
     }
