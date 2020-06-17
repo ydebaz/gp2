b70aaf573c75dc297894ec834d07ea1ed3786798
Yousef debaz <y.debaz@hotmail.com>
2020-06-17 18:14:04 +0300

    unit test1

--- gp_/test/UnitTest1.cs
+++ gp_/test/UnitTest1.cs
@@ -1,18 +1,20 @@
 using NUnit.Framework;
-
+using gp_;
 namespace test
 {
     public class Tests
     {
+        gp_.Controllers.errorController ec;
         [SetUp]
         public void Setup()
         {
+            ec = new gp_.Controllers.errorController();
         }
 
         [Test]
         public void Test1()
         {
-         
+            ec.httpstatuscodehandler(404);
         }
     }
 }
