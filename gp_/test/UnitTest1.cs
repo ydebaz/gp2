9ae8f894b5c663bb691d47f38d67c6a08c41dfdb
Yousef debaz <y.debaz@hotmail.com>
2020-06-17 17:47:45 +0300

    nunit

---
+++ gp_/test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using NUnit.Extensions.Asp;
+using NUnit.Extensions.Asp.AspTester;
+namespace test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+         
+        }
+    }
+}
